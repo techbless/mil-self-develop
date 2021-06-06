311eddcf751c1e45706235649ee466755552e745
Yunbin Chang <yunbin@hansung.ac.kr>
2021-06-06 23:20:04 +0900

    Fix escaping problem

--- src/controllers/article.ts
+++ src/controllers/article.ts
@@ -4,7 +4,48 @@ import ArticleService from '../services/article';
 
 class ArticleController {
   public async getEditor(req: Request, res: Response) {
-    res.render('article/editor')
+    const userId: number = req.user?.userId as number;
+    const isUpdating = req.query.update;
+
+    if(isUpdating === 'true') {
+      const articleIdInString = req.query.articleId;
+      
+      if(articleIdInString === '') {
+        res.send("Article Id is not sepecified");
+        return;
+      }
+
+      const articleId: number = +articleIdInString!;
+      const article = await ArticleService.getArticle(articleId);
+
+      const author = await article?.getUser();
+      if(author?.userId === req.user?.userId) {
+        res.render('article/editor', {
+          updating: true,
+          article,
+        });
+      } else {
+        res.render('article/view', {
+          title: 'Not Authorized',
+          markdown: "# You don't have it",
+        })
+      }
+
+      return;
+    }
+
+    const article = await ArticleService.getTodayArticle(userId);
+    if(article) {
+      res.render('article/editor', {
+        updating: true,
+        article,
+      });
+    } else {
+      res.render('article/editor', {
+        updating: false,
+        article: null,
+      });
+    }
   }
 
   public getCalendar(req: Request, res: Response) {
@@ -17,31 +58,69 @@ class ArticleController {
       const articleId = +articleIdInString;
   
       const result = await ArticleService.getArticle(articleId);
-      const escapedMarkdown = result?.markdown
-        .replace(/&/g, "&amp;")
-        .replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;")
-        .replace(/"/g, "&quot;")
-        .replace(/'/g, "&#039;")
-        .replace(/`/g, "&#96;");
+
+      const author = await result?.getUser();
+      if(author?.userId === req.user?.userId) {
+        const escapedMarkdown = result?.markdown
+          .replace(/&/g, "&amp;")
+          .replace(/</g, "&lt;")
+          .replace(/>/g, "&gt;")
+          .replace(/"/g, "&quot;")
+          .replace(/'/g, "&#039;")
+          .replace(/`/g, "&#96;");
+  
+        res.render('article/view', {
+          title: result?.subject,
+          markdown: escapedMarkdown,
+        })
+
+        return;
+      }
 
       res.render('article/view', {
-        title: result?.subject,
-        markdown: escapedMarkdown,
+        title: 'Not Authorized',
+        markdown: '# You are not the author',
       })
+
     } catch(err) {
       res.send(err);
     }
+  }
+
+  public async getArticles(req: Request, res: Response) {
+    const userId: number = req.user?.userId as number;
+    const monthInString: string = req.query.month as string;
+    const month: number = +monthInString;
+    const yearInString: string = req.query.year as string;
+    const year: number = +yearInString;
+
+    const result = await ArticleService.getArticlesByMonth(userId, year, month);
+    res.json(result);
   }
 
   public async postArticle(req: Request, res: Response) {
       const userId: number = req.user?.userId as number;
       const subject: string = req.body.subject;
       const markdown: string = req.body.markdown;
+      const articleIdInString: string = req.body.articleId;
+      let articleId: number = +articleIdInString;
+
 
       try {
-        const result = await ArticleService.addArticleTo(userId, subject, markdown);
-        res.redirect(`/article?articleId=${result.articleId}`);
+        let result;
+        if(articleId === -999) {
+          result = await ArticleService.addArticleTo(userId, subject, markdown);
+          articleId = result.articleId;
+        } else {
+          const article = await ArticleService.getArticle(articleId);
+          const user = await article?.getUser();
+
+          if(user?.userId == userId) {
+            result = await ArticleService.updateArticle(userId, articleId, subject, markdown);
+          }
+        }
+
+        res.redirect(`/article?articleId=${articleId}`);
       } catch(err) {
         res.send(err);
         return;
