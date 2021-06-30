85fec4d78c7db7a3c6f22c31d84260cef2cf299f
Yunbin Chang <yunbin@hansung.ac.kr>
2021-06-30 15:31:35 +0900

    Make a verification mail look great

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,59 +1,71 @@
-import { Request, Response, NextFunction } from 'express';
-import { IVerifyOptions } from 'passport-local';
-import User from '../models/user';
-import { Purpose } from '../models/token';
+import { Request, Response, NextFunction } from "express";
+import { IVerifyOptions } from "passport-local";
+import User from "../models/user";
+import { Purpose } from "../models/token";
 
-import passport = require('passport');
+import passport = require("passport");
 
-import UserService from '../services/user';
-import TokenService from '../services/token';
-import EmailService from '../services/email';
-
+import UserService from "../services/user";
+import TokenService from "../services/token";
+import EmailService from "../services/email";
 
 class UserController {
   public getLogin = (req: Request, res: Response) => {
     if (req.user) {
-      return res.redirect('/');
+      return res.redirect("/");
     }
-    res.render('account/login', {
-      title: 'Login',
+    res.render("account/login", {
+      title: "Login",
     });
   };
 
   public postLogin = (req: Request, res: Response, next: NextFunction) => {
-    passport.authenticate('local', (err: Error, user: User, info: IVerifyOptions) => {
-      if (err) { return next(err); }
-      if (!user) { return res.redirect('/login'); }
+    passport.authenticate(
+      "local",
+      (err: Error, user: User, info: IVerifyOptions) => {
+        if (err) {
+          return next(err);
+        }
+        if (!user) {
+          return res.redirect("/login");
+        }
 
-      // eslint-disable-next-line no-shadow
-      req.logIn(user, (err) => {
-        if (err) { return next(err); }
-        res.redirect('/');
-      });
-    })(req, res, next);
-  }
+        // eslint-disable-next-line no-shadow
+        req.logIn(user, (err) => {
+          if (err) {
+            return next(err);
+          }
+          res.redirect("/");
+        });
+      }
+    )(req, res, next);
+  };
 
   public getRegister = (req: Request, res: Response) => {
-    res.render('account/register', {
-      title: 'Register',
+    res.render("account/register", {
+      title: "Register",
     });
-  }
+  };
 
-
-  public getVerify = async(req: Request, res: Response) => {
+  public getVerify = async (req: Request, res: Response) => {
     const { email, token, purpose } = req.query;
-    
 
-    if(!await TokenService.verifyToken(email as string, token as string, purpose as Purpose, true)) {
+    if (
+      !(await TokenService.verifyToken(
+        email as string,
+        token as string,
+        purpose as Purpose,
+        true
+      ))
+    ) {
       console.log("failed");
-      res.send('인증 실패');
+      res.send("인증 실패");
       return;
     }
 
     UserService.verifyUser(email as string);
     res.send("인증 성공");
-  }
-
+  };
 
   public postRegister = async (req: Request, res: Response) => {
     const user: User = await User.create({
@@ -62,74 +74,68 @@ class UserController {
       password: req.body.password,
       rank: req.body.rank,
       isVerified: false,
-      unitId: (req.body.unit_id !== '') ? req.body.unit_id : null,
+      unitId: req.body.unit_id !== "" ? req.body.unit_id : null,
       score: 0,
     });
 
-    await EmailService.sendVerficationEmail(user, 'register');
+    await EmailService.sendVerficationEmail(user, "register");
 
     res.json(user);
-  }
-
+  };
 
   public getPasswordResetPage = async (req: Request, res: Response) => {
-    res.render('account/reset_password', {
-      title: 'Reset Password',
+    res.render("account/reset_password", {
+      title: "Reset Password",
       isVerified: false,
     });
-  }
+  };
 
-
-  public getSendPasswordMail = async(req: Request, res: Response) => {
+  public getSendPasswordMail = async (req: Request, res: Response) => {
     const email: string = req.query.email as string;
     const user = await UserService.getUserByEmail(email);
 
-    if(user) {
-      EmailService.sendVerficationEmail(user, 'reset');
+    if (user) {
+      EmailService.sendVerficationEmail(user, "reset");
       res.send("please check your inbox");
-    }
-    else {
+    } else {
       res.send("There is no such user");
     }
-  }
-
+  };
 
   public getResetPassword = async (req: Request, res: Response) => {
     const email: string = req.query.email as string;
     const token: string = req.query.token as string;
 
-    if(!await TokenService.verifyToken(email, token, 'reset', false)) {
+    if (!(await TokenService.verifyToken(email, token, "reset", false))) {
       res.send("Failed to verify");
       return;
     }
 
-    res.render('account/reset_password', {
-      title: 'Reset Password',
+    res.render("account/reset_password", {
+      title: "Reset Password",
       isVerified: true,
       email,
       token,
     });
-  }
-
+  };
 
   public postResetPassword = async (req: Request, res: Response) => {
     const email: string = req.body.email as string;
     const token: string = req.body.token as string;
     const password: string = req.body.password as string;
 
-    if(!await TokenService.verifyToken(email, token, 'reset', true)) {
+    if (!(await TokenService.verifyToken(email, token, "reset", true))) {
       res.send("Failed to verify");
       return;
     }
 
     await UserService.updatePassword(email, password);
     res.send("Password has been updated");
-  }
-
+  };
 
   public logout = (req: Request, res: Response) => {
     req.logout();
-    res.redirect('/');
+    res.redirect("/");
   };
 }
 
--- src/services/email.ts
+++ src/services/email.ts
@@ -1,27 +1,27 @@
-import * as ejs from 'ejs';
-import * as nodemailer from 'nodemailer';
+import * as ejs from "ejs";
+import * as nodemailer from "nodemailer";
 
-import { Purpose } from '../models/token';
-import User from '../models/user';
+import { Purpose } from "../models/token";
+import User from "../models/user";
 
-import TokenService from './token';
+import TokenService from "./token";
 
 class EmailService {
   private transporter: nodemailer.Transporter;
 
   constructor() {
     this.transporter = nodemailer.createTransport({
-        // 사용하고자 하는 서비스, gmail계정으로 전송할 예정이기에 'gmail'
-        service: 'gmail',
-        // host를 gmail로 설정
-        host: 'smtp.gmail.com',
-        port: 587,
-        secure: false,
-        auth: {
-          user: process.env.NODEMAILER_USER,
-          pass: process.env.NODEMAILER_PASSWORD,
-        },
-      });
+      // 사용하고자 하는 서비스, gmail계정으로 전송할 예정이기에 'gmail'
+      service: "gmail",
+      // host를 gmail로 설정
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.NODEMAILER_USER,
+        pass: process.env.NODEMAILER_PASSWORD,
+      },
+    });
   }
 
   private async sendEmail(email: string, subject: string, content: string) {
@@ -30,27 +30,38 @@ class EmailService {
       to: email,
       subject: subject,
       text: content,
-      html: `<b>${content}</b>`,
+      html: content,
     });
   }
-
 
   public async sendVerficationEmail(user: User, purpose: Purpose) {
     const token: string = await TokenService.issueToken(user.userId, purpose);
 
-    let url;
-    switch(purpose) {
-      case 'register':
-        url = `localhost:3000/verify?email=${user.email}&token=${token}&purpose=register`;
-        this.sendEmail(user.email, '회원가입 인증', url);
+    let url, purposeInKorean;
+    switch (purpose) {
+      case "register":
+        url = `http://localhost:3000/verify?email=${user.email}&token=${token}&purpose=register`;
+        purposeInKorean = "회원가입 인증";
+        //this.sendEmail(user.email, "회원가입 인증", url);
         break;
-      case 'reset':
-        url = `localhost:3000/reset_password?email=${user.email}&token=${token}`
-        this.sendEmail(user.email, '비밀번호 재설정', url);
+      case "reset":
+        url = `http://localhost:3000/reset_password?email=${user.email}&token=${token}`;
+        purposeInKorean = "비밀번호 초기화";
+        //this.sendEmail(user.email, "비밀번호 재설정", url);
         break;
       default:
-        break;
+        return;
     }
+
+    const html = await ejs.renderFile(
+      `${__dirname}/../templates/verification.ejs`,
+      {
+        purpose: purposeInKorean,
+        url: url,
+      }
+    );
+
+    this.sendEmail(user.email, purposeInKorean, html);
   }
 }
 
