72723ad235f5425f32695a2f76805f6b8d480964
Yunbin Chang <yunbin@hansung.ac.kr>
2021-07-02 14:44:27 +0900

    Design rank page

--- src/controllers/rank.ts
+++ src/controllers/rank.ts
@@ -1,31 +1,41 @@
-import { Request, Response } from 'express';
-import RankService from '../services/rank';
-
+import { Request, Response } from "express";
+import RankService from "../services/rank";
 
 class RankController {
   public async getRank(req: Request, res: Response) {
     if (!req.user) {
-      return res.redirect('/login');
+      return res.redirect("/login");
     }
 
     const rankInList = await RankService.getPersonalRank();
 
-    res.render('rank', {
-      title: 'Ranking',
+    let myRank = -1;
+    let i = 0;
+    for (const u of rankInList) {
+      if (u.userId == req.user.userId) {
+        myRank = i + 1;
+        break;
+      }
+      i++;
+    }
+    console.log(myRank);
+    res.render("rank", {
+      myRank,
+      user: req.user,
+      title: "Ranking",
       rankInList: rankInList,
     });
   }
 
-
   public async getUnitRank(req: Request, res: Response) {
-    if(!req.user) {
-      return res.redirect('/login');
+    if (!req.user) {
+      return res.redirect("/login");
     }
 
     const rankInList = await RankService.getUnitRank();
-    
-    res.render('unit_rank', {
-      title: 'Ranking',
+
+    res.render("unit_rank", {
+      title: "Ranking",
       rankInList: rankInList,
     });
   }
